4a013ddcdd5eda9d94bef7b7c0e81eeb8ced9f5c
anjalinairandayil <anjalinair.a@gmail.com>
2020-04-05 07:39:17 +0530

    Added Assignment

---
+++ Assignment 2/Average_3_numbers.py
@@ -0,0 +1,11 @@
+# calculate the average of 3 numbers
+
+data1 = int (input("Enter the first number : "))
+data2 = int (input("Enter the first number : "))
+data3 = int (input("Enter the first number : "))
+
+print ("The numbers entered are : " , data1,",",data2,",",data3)
+
+calc = (data1+data2+data3)/3
+
+print ("The average is : ", calc)
