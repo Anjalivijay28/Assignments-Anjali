21a8b0876f0406e5bdf4106cdf3545f7266dd688
anjalinairandayil <anjalinair.a@gmail.com>
2020-04-19 09:32:53 +0530

    Added Assignment-calc

---
+++ Assignment 19042020/ROI.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class RBI(ABC):
+    @abstractmethod
+    def getROI(self):
+        pass
+
+
+class SBI(RBI):
+    def __init__(self):
+        print("This is SBI class")
+    def getROI(self):
+        print("ROI is 2%")
+
+
+class HDFC(RBI):
+    def __init__(self):
+        print("This is HDFC class")
+
+    def getROI(self):
+        print("ROI is 4%")
+
+
+
+c1= SBI()
+c1.getROI()
+
+c2=HDFC()
+c2.getROI()
